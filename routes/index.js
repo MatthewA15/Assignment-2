8c2714afc31cfcab1f901116fbfea4e9f80ec26e
Matthew A <matthew.allicock@ontariotechu.net>
2023-10-25 13:30:53 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home Page',
+  heading: 'Home'
+ });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me',
+  heading: 'About'
+ });
+});
+
+/* GET Projects page. */
+router.get('/Projects', function(req, res, next) {
+  res.render('index', { title: 'Projects',
+  heading: 'Projects'
+ });
+});
+
+/* GET Contact page. */
+router.get('/Contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Us',
+  heading: 'Contact Us'
+ });
+});
+
+module.exports = router;
